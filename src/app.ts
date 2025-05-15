05188d807ea63cf955e0effdd3f3313dee15f15d
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-05-16 03:31:19 +0900

    feat:user_ifo_get_patch
    파일 수정: authDto, userStruct, app, hash
    파일 생성: user - controller, repository, router, service, type

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { defaultNotFoundHandler, globalErrorHandler } from './controllers/errorC
 import { authRouter } from './routers/authRouter';
 import memberRouter from './routers/memberRouter';
 import { PORT } from './lib/constants';
+import { userRouter } from './routers/userRouter';
 
 const app: express.Application = express();
 app.use(express.json());
 
 app.use('/auth', authRouter);
 app.use('/email', memberRouter);
+app.use('/users', userRouter);
 
 app.use(defaultNotFoundHandler);
 app.use(globalErrorHandler);
